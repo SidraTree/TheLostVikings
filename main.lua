fc0db85cfb54d3ed69ab06deb6f197ec2c074ee4
SidraTree <sidrareatherford@gmail.com>
2025-02-15 00:01:11 -0600

    Initial Upload
    Contains the original project and the barest of skeletons for the rework

---
+++ main.lua
@@ -0,0 +1,12 @@
+--[[-----------------------------------------------------
+THE LOST VIKINGS
+------------------------------------------------------]]--
+function love.load(args, unfilteredArgs)
+	love.window.setTitle("Lost Vikings")
+end
+
+function love.update(dt)
+end
+
+function love.draw() 
+end
