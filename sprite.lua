e1e08a134fcc7dffffc8e4d78a682d47b3a6f9c8
SidraTree <sidrareatherford@gmail.com>
2025-02-22 00:03:05 -0600

    Ok, I just manually forced everything to work right...

---
+++ sprite.lua
@@ -0,0 +1,52 @@
+--[[-----------------------------------------------------
+PROJCT VIKING
+-Sprite
+--Animatable Sprites
+------------------------------------------------------]]--
+
+facing = {
+	north = {},
+	northwest = {},
+	northeast = {},
+	south = {},
+	southwest = {},
+	southeast = {},
+	east = {},
+	west = {}
+	}
+
+function createSprite (aImage, aX, aY, aSpeed)
+	if aX == nil then aX = 0 end
+	if aY == nil then aY = 0 end
+	if aSpeed == nil then aSpeed = 0 end
+	return {image = love.graphics.newImage(aImage), x = aX, y = aY, speed = aSpeed}
+end
+
+function drawSprite (sprite)
+	love.graphics.draw(sprite.image, sprite.x, sprite.y)
+end
+
+function moveSprite(aSprite, aFacing, dt)
+	if aFacing == facing.north then
+		aSprite.y = aSprite.y - (aSprite.speed * dt)
+	elseif aFacing == facing.northwest then
+		aSprite.x = aSprite.x - (aSprite.speed * dt)
+		aSprite.y = aSprite.y - (aSprite.speed * dt)
+	elseif aFacing == facing.northeast then
+		aSprite.x = aSprite.x + (aSprite.speed * dt)
+		aSprite.y = aSprite.y - (aSprite.speed * dt)
+	elseif aFacing == facing.south then
+		aSprite.y = aSprite.y + (aSprite.speed * dt)
+	elseif aFacing == facing.southwest then
+		aSprite.x = aSprite.x - (aSprite.speed * dt)
+		aSprite.y = aSprite.y + (aSprite.speed * dt)
+	elseif aFacing == facing.southeast then
+		aSprite.x = aSprite.x + (aSprite.speed * dt)
+		aSprite.y = aSprite.y + (aSprite.speed * dt)
+	elseif aFacing == facing.west then
+		aSprite.x = aSprite.x - (aSprite.speed * dt)
+	elseif aFacing == facing.east then
+		aSprite.x = aSprite.x + (aSprite.speed * dt)
+	end
+	
+end
